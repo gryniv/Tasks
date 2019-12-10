02b6e44360dd2246e60dd3523e9893162f25f410
Dmytro Derzhevytskyi <Dmytro.Derzhevytskyi@epam.com>
2019-12-10 08:39:30 +0200

    1.2.1 Added divide test case

--- calculator/src/test/java/org/training/calculator/MathCalculatorTest.java
+++ calculator/src/test/java/org/training/calculator/MathCalculatorTest.java
@@ -1,25 +1,36 @@
 package org.training.calculator;
 
-import org.junit.Assert;
+import java.io.IOException;
+
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
+import static org.junit.Assert.assertEquals;
 
 public class MathCalculatorTest {
+    public static final String EXPECTED_DIVIDE_RESULT = "1.5";
     String littleEquation;
     String bigEquation;
-    Calculator calculator = new MathCalculator(new MathOperations());
+    private Calculator calculator;
+    private static final String DIVIDE_OPERATION = "3/2";
 
     @Before
     public void setUp(){
+        calculator = new MathCalculator(new MathOperations());
     littleEquation = "2+2*2";
     bigEquation = "2+2+2+2*2";
     }
 
     @Test
     public void calculate() throws IOException {
-        Assert.assertEquals(littleEquation + " Must be equal 6", "6", calculator.calculate(littleEquation));
-        Assert.assertEquals(bigEquation + " Must be equal 10", "10", calculator.calculate(bigEquation));
+        assertEquals(littleEquation + " Must be equal 6", "6", calculator.calculate(littleEquation));
+        assertEquals(bigEquation + " Must be equal 10", "10", calculator.calculate(bigEquation));
+    }
+
+    @Test
+    public void shouldDivide() throws IOException {
+        final String result = calculator.calculate(DIVIDE_OPERATION);
+
+        assertEquals(EXPECTED_DIVIDE_RESULT, result);
     }
 }
