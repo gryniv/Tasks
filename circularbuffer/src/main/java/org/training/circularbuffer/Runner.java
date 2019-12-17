f26d21956d7b86d910f1e200f18efe8e8b523d2a
Dmytro Derzhevytskyi <Dmytro.Derzhevytskyi@epam.com>
2019-12-17 20:57:46 +0200

    1.1.1 generic puzzle v2

--- circularbuffer/src/main/java/org/training/circularbuffer/Runner.java
+++ circularbuffer/src/main/java/org/training/circularbuffer/Runner.java
@@ -1,25 +1,21 @@
 package org.training.circularbuffer;
 
+import java.util.Arrays;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.training.circularbuffer.service.ArrayCircularBuffer;
-
-import java.util.Arrays;
 
 public class Runner {
     private static final Logger LOG = LogManager.getLogger(Runner.class);
 
     public static void main(String[] args) {
         LOG.info("Enter an expression: ");
-        ArrayCircularBuffer<String> buffer1 = new ArrayCircularBuffer<>(10);
-        ArrayCircularBuffer<String> buffer2 = new ArrayCircularBuffer<>(10, String.class); // this work correct
-        buffer1.put("0");
-        buffer2.put("0");
-        buffer1.put("1");
+        ArrayCircularBuffer<String> buffer2 = new ArrayCircularBuffer<>(10); // this work correct
         buffer2.put("1");
+        buffer2.put("2");
         String[] asArray2 = buffer2.toArray();
-        buffer1.put("1");
-        System.out.println(Arrays.toString(asArray2));
+        LOG.debug(Arrays.toString(asArray2));
     }
 }
 
